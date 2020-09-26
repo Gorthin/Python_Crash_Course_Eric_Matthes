72f2dbeed31c9d50e37319d8adc610c98fa5326e
Adrian88 <adrian88szymanski@gmail.com>
2020-09-26 11:24:53 +0200

    First program from Chapter 2.

---
+++ 2.1_2.2.py
@@ -0,0 +1,12 @@
+#! python3
+
+print("2.1")
+message = "My first variable"
+print(message)
+
+print("2.2")
+message2 = "I  learn Python programming"
+print(message2)
+
+message2 = "I  use Pythonn programming for to do my program"
+print(message2)
---
+++ 2.8_2.9.py
@@ -0,0 +1,15 @@
+#! python3
+
+print("2.8")
+print(4 + 4)
+print(12 - 4)
+print(2 * 4)
+print(16 / 2)
+
+print("2.9")
+my_number = 8
+print("My favorite number is:", my_number)
+print("Or:")
+fav_num = 3
+message = f"My favorite number is {fav_num}."
+print(message)
