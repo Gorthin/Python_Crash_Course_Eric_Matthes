1e3f66d0b54691f59733bf7585f9e7555bf6f1a7
Adrian88 <adrian88szymanski@gmail.com>
2020-09-26 12:26:42 +0200

    New program from Chapter 3.

---
+++ Chapter 3/3.1_3.2_3.3.py
@@ -0,0 +1,23 @@
+#! python3
+
+print("3.1")
+names = ['Bob', 'Kate', 'Mary', 'Peter', 'John']
+print(names[0])
+print(names[1])
+print(names[2])
+print(names[3])
+print(names[4])
+
+print("3.2")
+names = ['Bob', 'Kate', 'Mary', 'Peter', 'John']
+print("Hello! ", names[0])
+print("Hello! ", names[1])
+print("Hello! ", names[2])
+print("Hello! ", names[3])
+print("Hello! ", names[4])
+msg = "Hello! "
+print(msg, names[0])
+
+print("3.3")
+vehicles = ['car', 'bike', 'plane']
+print("I want to buy a ", vehicles[2])
