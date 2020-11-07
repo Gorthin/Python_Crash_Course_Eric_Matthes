d3e8f98cfb4e9d33df7854036e2b3b75a0ec4e79
Adrian88 <adrian88szymanski@gmail.com>
2020-11-07 14:04:17 +0100

    Commit alien_invasion.py ver4

--- alien_invasion/alien_invasion.py
+++ alien_invasion/alien_invasion.py
@@ -5,6 +5,7 @@ import sys
 import pygame
 
 from settings import Settings
+from ship import Ship
 
 class AlienInvasion:
     """A class dedicated to managing the resources and the way the game works."""
@@ -17,6 +18,8 @@ class AlienInvasion:
         self.screen = pygame.display.set_mode((self.settings.screen_width, self.settings.screen_height))
         pygame.display.set_caption("Alien invasion")
 
+        self.ship = Ship(self)
+
     def run_game(self):
         """Starting the main loop of the program."""
         while True:
@@ -27,6 +30,7 @@ class AlienInvasion:
 
             #Refresh the screen during each iteration of the loop
             self.screen.fill(self.settings.bg_color)
+            self.ship.blitme()
 
             #Waiting for a key or mouse button to be pressed.
             pygame.display.flip()
