8b58649356a39f1cf1c831aeef61970ec169be6d
Adrian88 <adrian88szymanski@gmail.com>
2020-11-07 13:45:56 +0100

    Commit settings.py ver1

---
+++ alien_invasion/settings.py
@@ -0,0 +1,11 @@
+#! python3
+
+class Settings:
+    """The class is designed to store all game settings."""
+
+    def __init__(self):
+        """Initializing the game settings."""
+        #Setting of screen
+        self.screen_width = 1200
+        self.screen_height = 800
+        self.bg_color = (230, 230, 230)
