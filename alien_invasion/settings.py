8145a003afe757414a133ed8eee86edce5ef8ddd
Adrian88 <adrian88szymanski@gmail.com>
2020-11-07 14:31:00 +0100

    Commit settings.py.py ver2

--- alien_invasion/settings.py
+++ alien_invasion/settings.py
@@ -8,4 +8,4 @@ class Settings:
         #Setting of screen
         self.screen_width = 1200
         self.screen_height = 800
-        self.bg_color = (230, 230, 230)
+        self.bg_color = (135, 206, 250)
