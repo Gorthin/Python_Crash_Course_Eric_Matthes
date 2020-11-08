1ec621d906b2ea5a420f6b757acb4630ec9a4d97
AdrianSz <adrian88szymanski@gmail.com>
2020-11-08 12:58:17 +0100

    Change alien_invasion.py ver 6

--- alien_invasion/alien_invasion.py
+++ alien_invasion/alien_invasion.py
@@ -24,6 +24,7 @@ class AlienInvasion:
         """Starting the main loop of the program."""
         while True:
             self._check_events()
+            self.ship.update()
             self._update_screen()
 
     def _check_events(self):
@@ -31,6 +32,27 @@ class AlienInvasion:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 sys.exit()
+            elif event.type == pygame.KEYDOWN:
+                self._check_keydown_events(event)
+            elif event.type == pygame.KEYUP:
+                self._check_keyup_events(event)
+
+    def _check_keydown_events(self, event):
+        """Response to pressing a key."""
+        if event.key == pygame.K_RIGHT:
+            self.ship.moving_right = True
+        elif event.key == pygame.K_LEFT:
+            self.ship.moving_left = True
+        elif event.key == pygame.K_q:
+            sys.exit()
+
+    def _check_keyup_events(self, event):
+        """Reaction to the release of the key."""
+        if event.key == pygame.K_RIGHT:
+            self.ship.moving_right = False
+        elif event.key == pygame.K_LEFT:
+            self.ship.moving_left = False
+
 
     def _update_screen(self):
         """Updates the images on the screen and goes to a new screen."""
