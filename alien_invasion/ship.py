6eed44afba7a06f49f3a66cee8829ec9630dce3b
AdrianSz <adrian88szymanski@gmail.com>
2020-11-08 12:57:13 +0100

    Change ship.py ver 3

--- alien_invasion/ship.py
+++ alien_invasion/ship.py
@@ -9,6 +9,7 @@ class Ship():
         """Spaceship initialization and its initial position."""
 
         self.screen = ai_game.screen
+        self.settings = ai_game.settings
         self.screen_rect = ai_game.screen.get_rect()
 
         #Load a spaceship image and retrieve its rectangle.
@@ -17,6 +18,25 @@ class Ship():
 
         #Each new ship appears in the bottom center of the screen.
         self.rect.midbottom = self.screen_rect.midbottom
+
+        #The ship's horizontal position is stored as a floating point number.
+        self.x = float(self.rect.x)
+
+        #Options that indicate the movement of the ship
+        self.moving_right = False
+        self.moving_left = False
+
+    def update(self):
+        """Update the ship's position based on the option that indicates its movement."""
+
+        #Updating the X coordinate of the ship, not its rectangle
+        if self.moving_right and self.rect.right < self.screen_rect.right:
+            self.x += self.settings.ship_speed
+        if self.moving_left and self.rect.left > 0:
+            self.x -= self.settings.ship_speed
+
+        #Updating rect based on self.x
+        self.rect.x = self.x
 
     def blitme(self):
         """Displaying a spaceship in its current position."""
