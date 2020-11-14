23af3d8fc457d3e3b1c81e90a5848b64fcd9d47f
Adrian88 <adrian88szymanski@gmail.com>
2020-11-14 10:31:13 +0100

    Change alien.py ver 2

--- alien_invasion/alien.py
+++ alien_invasion/alien.py
@@ -10,6 +10,7 @@ class Alien(Sprite):
         """Initialization alien and definition his starting position"""
         super().__init__()
         self.screen = ai_game.screen
+        self.settings = ai_game.settings
 
         #Reading the image and defining its rect attribute.
         self.image = pygame.image.load('images/ship.bmp')
@@ -21,3 +22,14 @@ class Alien(Sprite):
 
         #Storing the exact horizontal foreign position
         self.x = float(self.rect.x)
+
+    def update(self):
+        """Move the alien to the right"""
+        self.x += (self.settings.alien_speed * self.settings.fleet_direction)
+        self.rect.x = self.x
+
+    def check_edges(self):
+        """Returns True if the alien reaches the edge of the screen"""
+        screen_rect = self.screen.get_rect()
+        if self.rect.right >= screen_rect.right or self.rect.left <= 0:
+            return True
