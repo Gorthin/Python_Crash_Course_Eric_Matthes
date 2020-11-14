d2d9d74105826e7b6455051cc8fef63191723e23
Adrian88 <adrian88szymanski@gmail.com>
2020-11-14 10:32:07 +0100

    Change settings.py ver 5

--- alien_invasion/settings.py
+++ alien_invasion/settings.py
@@ -10,10 +10,16 @@ class Settings:
         self.screen_height = 800
         self.bg_color = (135, 206, 250)
         self.ship_speed = 1.5
+        self.ship_limit = 3
 
         #bullet
-        self.bullet_speed = 1.0
+        self.bullet_speed = 1.5
         self.bullet_width = 3
         self.bullet_height = 15
         self.bullet_color = (255, 0, 0)
         self.bullets_allowed = 4
+
+        #Alien
+        self.alien_speed = 1.0
+        self.fleet_drop_speed = 10
+        self.fleet_direction = 1
