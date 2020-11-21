4db5ddc9cc8498c26917a67c9ee2a70d3741dd36
Adrian88 <adrian88szymanski@gmail.com>
2020-11-21 11:35:39 +0100

    Change game_stats.py ver 2

--- alien_invasion/game_stats.py
+++ alien_invasion/game_stats.py
@@ -1,14 +1,21 @@
 #! python3
 
 class GameStats:
-    """In-game statistics monitoring in 'Aliens invasion'"""
+    """Track statistics for Alien Invasion."""
 
     def __init__(self, ai_game):
-        """Initialization in-game statistic"""
+        """Initialize statistics."""
         self.settings = ai_game.settings
         self.reset_stats()
-        self.game_active = True
+
+        # Start game in an inactive state.
+        self.game_active = False
+
+        # High score should never be reset.
+        self.high_score = 0
 
     def reset_stats(self):
-        """Initialization in-game statistic, wich can change during game"""
-        self.ship_left = self.settings.ship_limit
+        """Initialize statistics that can change during the game."""
+        self.ships_left = self.settings.ship_limit
+        self.score = 0
+        self.level = 1
