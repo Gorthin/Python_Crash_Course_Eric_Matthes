4c73eec14975285d735c83f776e7f96019fc92eb
Adrian88 <adrian88szymanski@gmail.com>
2021-01-30 10:03:59 +0100

    New data from chapter 19

--- learning_log/learning_log/settings.py
+++ learning_log/learning_log/settings.py
@@ -32,6 +32,7 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'learning_logs',
+    'users',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
--- learning_log/learning_logs/models.py
+++ learning_log/learning_logs/models.py
@@ -2,8 +2,8 @@ from django.db import models
 
 class Topic(models.Model):
     """The topic the user is learning about."""
-    text = models.CharField(max_length = 200)
-    date_added = models.DateTimeField(auto_now_add = True)
+    text = models.CharField(max_length=200)
+    date_added = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
         """Returns the model as text."""
@@ -11,9 +11,9 @@ class Topic(models.Model):
 
 class Entry(models.Model):
     """Information about progress in science"""
-    topic = models.ForeignKey(Topic, on_delete = models.CASCADE)
+    topic = models.ForeignKey(Topic, on_delete=models.CASCADE)
     text = models.TextField()
-    date_added = models.DateTimeField(auto_now_add = True)
+    date_added = models.DateTimeField(auto_now_add=True)
 
     class Meta:
         verbose_name_plural = 'entries'
