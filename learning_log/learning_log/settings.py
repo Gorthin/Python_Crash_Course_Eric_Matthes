ac517ab61bb12f849ec4fd98d294a832b8820142
Adrian88 <adrian88szymanski@gmail.com>
2021-02-06 12:59:24 +0100

    New data from chapter 20

--- learning_log/learning_log/settings.py
+++ learning_log/learning_log/settings.py
@@ -33,6 +33,7 @@ ALLOWED_HOSTS = []
 INSTALLED_APPS = [
     'learning_logs',
     'users',
+    'bootstrap4',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
